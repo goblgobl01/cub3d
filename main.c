17c285e61be9d0069177f63ede8f3a3d8dbd1d74
goblgobl01 <maarafimohammed@gmail.com>
2025-10-13 15:57:33 +0100

    alo

--- main.c
+++ main.c
@@ -6,11 +6,76 @@
 /*   By: mmaarafi <mmaarafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:06:28 by mmaarafi          #+#    #+#             */
-/*   Updated: 2025/10/13 15:06:29 by mmaarafi         ###   ########.fr       */
+/*   Updated: 2025/10/13 15:55:19 by mmaarafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+
+void	free_everything(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	if (data->map)
+	{
+		while (data->map[i])
+		{
+			free(data->map[i]);
+			i++;
+		}
+		free(data->map);
+	}
+	if (data->big_line)
+		free (data->big_line);
+	if (data->NO)
+		free (data->NO);
+	if (data->SO)
+		free (data->SO);
+	if (data->WE)
+		free (data->WE);
+	if (data->EA)
+		free (data->EA);
+	if (data)
+		free(data);
+}
+
+int	inside_charset(char c)
+{
+	int	i;
+	char *charset = " \f\n\r\t\v";
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_words(char *str)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (inside_charset(str[i]))
+		{
+			i++;
+			continue ;
+		}
+		while (str[i] && !inside_charset(str[i]))
+			i++;
+		count++;
+	}
+	return (count);
+}
+
 
 int	checking_arguments(char *str)
 {
@@ -49,6 +114,38 @@ void	intializing_all_variables(t_data **data)
 	(*data)->map_width = 0;
 }
 
+void initializing_textures_path(int fd, t_data *data)
+{
+	char	*ptr;
+	int		fd;
+	int		length;
+
+	while ((ptr = get_next_line(fd)))
+	{
+		if (*ptr == '\n')
+		{
+			free(ptr);
+			continue ;
+		}
+		else
+		{
+			if (count_words(ptr) == 2)
+			{
+				
+			}
+			else
+			{
+				free_everything(data);
+				exit(1);
+			}
+		}
+	}
+	// close(fd);
+	// if (!characters_checking(data->big_line, data))
+	// 	return (write(2, "Error\n", 6), free(data->big_line), 
+	// 		freedata, exit(1), (void)0);
+}
+
 void reading_map_file(char *str, t_data *data)
 {
 	char	*ptr;
@@ -58,7 +155,7 @@ void reading_map_file(char *str, t_data *data)
 	fd = open(str, O_RDONLY);
 	if (fd < 0)
 		return (write(2, "Error\n", 6), free(data), exit(1), (void)0);
-	intializing_textures(fd);
+	intializing_textures_path(fd);
 }
 
 int main(int ac, char **av)
@@ -84,5 +181,5 @@ int main(int ac, char **av)
 // Except for the map content, each type of element can be separated by one or more empty lines.
 // Except for the map content which always has to be the last, each type of element can be set in any order in the file.
 // Except for the map, each type of information from an element can be separated by one or more spaces.
-// each element must begin with its type identifier (composed by one or two characters), followed by its specific information in a strict order
-
+// each element must begin with its type identifier (composed by one or two characters),
+// followed by its specific information in a strict order
