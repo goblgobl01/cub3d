00d91388602764422c127c47a684c7452835dd26
goblgobl01 <maarafimohammed@gmail.com>
2025-10-13 15:11:08 +0100

    alo

--- header.h
+++ header.h
@@ -7,7 +7,18 @@
 
 typedef struct s_struct
 {
-	
+	char		**map;
+	char		*big_line;
+	char		*NO;
+	char		*SO;
+	char		*WE;
+	char		*EA;
+	char		player_direction;
+	int			parameters_count;
+	int			starting_position_x;
+	int			starting_position_y;
+	int			map_height;
+	int			map_width;
 }t_data;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmaarafi <mmaarafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:06:28 by mmaarafi          #+#    #+#             */
-/*   Updated: 2025/10/13 12:59:03 by mmaarafi         ###   ########.fr       */
+/*   Updated: 2025/10/13 15:06:29 by mmaarafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,23 @@ void	intializing_all_variables(t_data **data)
 {
 	(*data)->map = NULL;
 	(*data)->big_line = NULL;
-	(*data)->s_img = NULL;
-	(*data)->g_img = NULL;
-	(*data)->w_img = NULL;
-	(*data)->c_img = NULL;
-	(*data)->e_img = NULL;
-	(*data)->mlx = NULL;
-	(*data)->exit = 0;
-	(*data)->height = 0;
-	(*data)->player = 0;
-	(*data)->collectible = 0;
-	(*data)->ffc = 0;
-	(*data)->ffe = 0;
-	(*data)->move_count = 0;
+	(*data)->player_direction = NULL;
+	(*data)->starting_position_x = 0;
+	(*data)->starting_position_y = 0;
+	(*data)->map_height = 0;
+	(*data)->map_width = 0;
+}
+
+void reading_map_file(char *str, t_data *data)
+{
+	char	*ptr;
+	int		fd;
+	int		length;
+
+	fd = open(str, O_RDONLY);
+	if (fd < 0)
+		return (write(2, "Error\n", 6), free(data), exit(1), (void)0);
+	intializing_textures(fd);
 }
 
 int main(int ac, char **av)
@@ -73,4 +77,12 @@ int main(int ac, char **av)
 		exit(1);
 	}
 	intializing_all_variables(&data);
+	reading_map_file(av[1], &data);
 }
+
+// prerequisite:
+// Except for the map content, each type of element can be separated by one or more empty lines.
+// Except for the map content which always has to be the last, each type of element can be set in any order in the file.
+// Except for the map, each type of information from an element can be separated by one or more spaces.
+// each element must begin with its type identifier (composed by one or two characters), followed by its specific information in a strict order
+
