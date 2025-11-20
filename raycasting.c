8e2d49c939025741597ec3802dd284b9c7b0f4f2
goblgobl01 <133246212+goblgobl01@users.noreply.github.com>
2025-11-20 20:02:58 +0000

    alo

--- raycasting.c
+++ raycasting.c
@@ -6,55 +6,60 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 00:54:33 by codespace         #+#    #+#             */
-/*   Updated: 2025/11/11 16:18:22 by codespace        ###   ########.fr       */
+/*   Updated: 2025/11/11 17:11:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+void initialize_parameters(int x, t_data *data)
+{
+	data->camera_x = 2 * x / (double) screenWidth - 1;
+	data->ray_dir_x = data->dir_x + data->plane_x * data->camera_x;
+	data->ray_dir_y = data->dir_y + data->plane_y * data->camera_x;
+	data->map_x = data->starting_position_x;
+	data->map_y = data->starting_position_y;
+	if (data->ray_dir_x == 0)
+		data->delta_dist_x = 1e30;
+	else
+		data->delta_dist_x = fabs(1/data->ray_dir_x);
+	if (data->ray_dir_y == 0)
+		data->delta_dist_y = 1e30;
+	else
+		data->delta_dist_y = fabs(1/data->ray_dir_y);
+	data->hit = 0;
+	if (data->ray_dir_x < 0)
+	{
+		data->step_x= -1;
+		data->side_dist_x = (data->position_x - data->map_x) * data->delta_dist_x;
+	}
+	else
+	{
+		data->step_x= 1;
+		data->side_dist_x = (data->map_x + 1 - data->position_x) * data->delta_dist_x;
+	}
+	if (data->ray_dir_y < 0)
+	{
+		data->step_y= -1;
+		data->side_dist_y = (data->position_y - data->map_y) * data->delta_dist_y;
+	}
+	else
+	{
+		data->step_y = 1;
+		data->side_dist_y = (data->map_y + 1 - data->position_y) * data->delta_dist_y;
+	}
+}
+
 void raycasting(t_data *data)
 {
 	int	x;
 	intializing_raycasting_variables(data);
-	while(1)
+	while(1 /* here in case of one it would be depending on the which frame */)
 	{
 		x = 0;
 		while(x < screenWidth)
 		{
-			data->camera_x = 2 * x / (double) screenWidth - 1;
-			data->ray_dir_x = data->dir_x + data->plane_x * data->camera_x;
-			data->ray_dir_y = data->dir_y + data->plane_y * data->camera_x;
-			data->map_x = data->starting_position_x;
-			data->map_y = data->starting_position_y;
-			if (data->ray_dir_x == 0)
-				data->delta_dist_x = 1e30;
-			else
-				data->delta_dist_x = fabs(1/data->ray_dir_x);
-			if (data->ray_dir_y == 0)
-				data->delta_dist_y = 1e30;
-			else
-				data->delta_dist_y = fabs(1/data->ray_dir_y);
-			data->hit = 0;
-			if (data->ray_dir_x < 0)
-			{
-				data->step_x= -1;
-				data->side_dist_x = (data->position_x - data->map_x) * data->delta_dist_x;
-			}
-			else
-			{
-				data->step_x= 1;
-				data->side_dist_x = (data->map_x + 1 - data->position_x) * data->delta_dist_x;
-			}
-			if (data->ray_dir_y < 0)
-			{
-				data->step_y= -1;
-				data->side_dist_y = (data->position_y - data->map_y) * data->delta_dist_y;
-			}
-			else
-			{
-				data->step_y = 1;
-				data->side_dist_y = (data->map_y + 1 - data->position_y) * data->delta_dist_y;
-			}
+			initialize_parameters(x, data);
 			while (data->hit == 0)
 			{
 				if (data->side_dist_x < data->side_dist_y)
