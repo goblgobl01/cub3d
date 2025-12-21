98293d5dc5f9166960fddc77aecb4d3e72306194
goblgobl01 <maarafimohammed@gmail.com>
2025-12-21 16:31:25 +0100

    alo

--- main.c
+++ main.c
@@ -6,48 +6,67 @@
 /*   By: mmaarafi <mmaarafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:06:28 by mmaarafi          #+#    #+#             */
-/*   Updated: 2025/12/21 16:20:02 by mmaarafi         ###   ########.fr       */
+/*   Updated: 2025/12/21 16:30:09 by mmaarafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-#define MOVE_SPEED 0.1
+#define MOVE_SPEED 0.05
 #define ROTATION 0.1
 
-void move_player(void *param)
+void move(void *param)
 {
-    t_data *data = param;
-
-    // Use MLX42's built-in key state checker
-    if (mlx_is_key_down(data->mlx, MLX_KEY_W))
-    {
-        if (data->map[(int)data->position_y][(int)(data->position_x + data->dir_x * MOVE_SPEED)] == '0')
-            data->position_x += data->dir_x * MOVE_SPEED;
-        if (data->map[(int)(data->position_y + data->dir_y * MOVE_SPEED)][(int)data->position_x] == '0')
-            data->position_y += data->dir_y * MOVE_SPEED;
-    }
-    if (mlx_is_key_down(data->mlx, MLX_KEY_S))
-    {
-        if (data->map[(int)data->position_y][(int)(data->position_x - data->dir_x * MOVE_SPEED)] == '0')
-            data->position_x -= data->dir_x * MOVE_SPEED;
-        if (data->map[(int)(data->position_y - data->dir_y * MOVE_SPEED)][(int)data->position_x] == '0')
-            data->position_y -= data->dir_y * MOVE_SPEED;
-    }
-    
-    // ... Repeat for A, D, and Arrow Keys using mlx_is_key_down ...
-
-    if (mlx_is_key_down(data->mlx, MLX_KEY_LEFT))
-    {
-        double oldDirX = data->dir_x;
-        data->dir_x = data->dir_x * cos(-ROTATION) - data->dir_y * sin(-ROTATION);
-        data->dir_y = oldDirX * sin(-ROTATION) + data->dir_y * cos(-ROTATION);
-        // ... update plane ...
-    }
-
-    // After updating positions, redraw the frame
-    raycasting(data);
-}
+	t_data *data = (t_data *) param;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_W))
+	{
+		if (data->map[(int) data->position_y][(int) (data->position_x + data->dir_x * MOVE_SPEED)] == '0')
+			data->position_x += data->dir_x * MOVE_SPEED;
+		if (data->map[(int) (data->position_y + data->dir_y * MOVE_SPEED)][(int) data->position_x] == '0')
+			data->position_y += data->dir_y * MOVE_SPEED;
+	}
+	if (mlx_is_key_down(data->mlx, MLX_KEY_S))
+	{
+		if (data->map[(int) data->position_y][(int) (data->position_x - data->dir_x * MOVE_SPEED)] == '0')
+			data->position_x -= data->dir_x * MOVE_SPEED;
+		if (data->map[(int) (data->position_y - data->dir_y * MOVE_SPEED)][(int) data->position_x] == '0')
+			data->position_y -= data->dir_y * MOVE_SPEED;
+	}
+	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
+	{
+		if (data->map[(int) data->position_y][(int) (data->position_x + data->plane_x * MOVE_SPEED)] == '0')
+			data->position_x += data->plane_x * MOVE_SPEED;
+		if (data->map[(int) (data->position_y + data->plane_y * MOVE_SPEED)][(int) data->position_x] == '0')
+			data->position_y += data->plane_y * MOVE_SPEED;
+	}
+	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
+	{
+		if (data->map[(int) data->position_y][(int) (data->position_x - data->plane_x * MOVE_SPEED)] == '0')
+			data->position_x -= data->plane_x * MOVE_SPEED;
+		if (data->map[(int) (data->position_y - data->plane_y * MOVE_SPEED)][(int) data->position_x] == '0')
+			data->position_y -= data->plane_y * MOVE_SPEED;
+	}
+	// if (keydata.key == MLX_KEY_LEFT)
+	// {
+	// 	double oldDirX = data->dir_x;
+	// 	data->dir_x = data->dir_x * cos(-ROTATION) - data->dir_y * sin(-ROTATION);
+	// 	data->dir_y = oldDirX * sin(-ROTATION) + data->dir_y * cos(-ROTATION);
+	// 	double oldPlaneX = data->plane_x;
+	// 	data->plane_x = data->plane_x * cos(-ROTATION) - data->plane_y * sin(-ROTATION);
+	// 	data->plane_y = oldPlaneX * sin(-ROTATION) + data->plane_y * cos(-ROTATION);
+	// }
+	// if (keydata.key == MLX_KEY_RIGHT)
+	// {
+	// 	double oldDirX = data->dir_x;
+	// 	data->dir_x = data->dir_x * cos(ROTATION) - data->dir_y * sin(ROTATION);
+	// 	data->dir_y = oldDirX * sin(ROTATION) + data->dir_y * cos(ROTATION);
+	// 	double oldPlaneX = data->plane_x;
+	// 	data->plane_x = data->plane_x * cos(ROTATION) - data->plane_y * sin(ROTATION);
+	// 	data->plane_y = oldPlaneX * sin(ROTATION) + data->plane_y * cos(ROTATION);
+	// }
+	// debug_view(data);
+	raycasting(data);
+} 
 
 int main(int ac, char **av)
 {
@@ -69,8 +88,8 @@ int main(int ac, char **av)
 	intializing_all_variables(&data);
 	reading_map_file(av[1], data);
 	// line added by youssef:
-    mlx_loop_hook(data->mlx, &move, data);
-    	// line added by youssef:
+	mlx_loop_hook(data->mlx, &move, data);
+		// line added by youssef:
 	// mlx_loop_hook(data->mlx, raycasting_wrapper, data);
 	mlx_loop(data->mlx);
 	// lines added by youssef:
